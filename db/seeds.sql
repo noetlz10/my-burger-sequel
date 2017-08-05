9fd6badcf5577b0bc49be43508de648f0731ce25
Noe Tellez <noetlz10@gmail.com>
2017-08-04 22:28:09 -0500

    adding additional files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado-Queso Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hickory Smoked Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ghost Pepper Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fourth Of July Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tequila Lime Burger', true);
