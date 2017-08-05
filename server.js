cba7c04fcf69dba9e3cf7cc3d4dcf8d56a9a28fb
Noe Tellez <noetlz10@gmail.com>
2017-08-05 08:58:45 -0500

    Editing server.js to work with Sequelize

--- server.js
+++ server.js
@@ -2,28 +2,51 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var log = require('loglevel');
+var path = require('path');
 
+//Configure logging
+console.log('process.env.NODE_ENV (in server.js) = ' + process.env.NODE_ENV);
+if (process.env.NODE_ENV) { // Production
+    console.log("Setting log Level to ERROR");
+    log.setLevel("ERROR");
+} else { //Development
+    var level = process.env.LOG_LEVEL || "DEBUG";
+    console.log("Setting log level to " + level);
+    log.setLevel(level);
+}
+
+// Set up Express Application
+var app = express();
 var port = process.env.PORT || 3306;
 
-var app = express();
+// Requiring our models for syncing
+var db = require(path.join(__dirname, '/models'));
 
 // Serve static content for the app from the 'public' directory
 app.use(express.static(process.cwd() + '/public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
+//Override with POST having _method=PUT
 app.use(methodOverride('_method'));
 
-// Set Handlebars as the view engine
-var exphbs = require('express-handlebars');
+//Sets up Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Set Handlebars are the view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them
-var routes = require('./controllers/burger_controller.js');
+require('./controllers/burgers_controller.js')(app);
 
-app.use('/', routes);
+//Syncing our sequelize models and then starting our express app
+db.sequelize.sync().then(function() {
+    app.listen(port, function() {
+        console.log("My-burger-sequel is listening on PORT " + port);
 
-app.listen(port);
+    });
+});
